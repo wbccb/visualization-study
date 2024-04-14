3849c5c0383d2a1542fce69b173abad16a6b1030
wbccb <wbccb18814122@163.com>
2024-04-14 23:59:28 +0800

    svg(mini-project): 【rotate项目】初始化矩形rect

---
+++ svg/mini-project/move-scale-rotate-demo/src/components/Rect.tsx
@@ -0,0 +1,7 @@
+import { RectProps } from "../types";
+
+const Rect = (props: RectProps) => {
+  return <rect {...props}></rect>;
+};
+
+export { Rect };
---
+++ svg/mini-project/move-scale-rotate-demo/src/types/index.ts
@@ -0,0 +1,9 @@
+export type RectProps = {
+  width: number;
+  height: number;
+  x: number;
+  y: number;
+  stroke: string;
+  strokeWidth: number;
+  fill: string;
+}
